8192eaa50046b518a4cd2d958f4ffb18e117fe0d
j-kucera <j_kucera@utb.cz>
2023-04-06 14:04:14 +0200

    First commit test. New code.

--- hello_world.c
+++ hello_world.c
@@ -11,30 +11,35 @@ int main() {
     printf("deleni:\n");
 
     char operace = 0;
-    int cisloA=0, cisloB=0;
-    float vysledek=0;
+    int cisloA = 0, cisloB = 0;
+    float vysledek = 0;
 
     operace = getchar();
     printf("zadej prvni cislo:");
-    scanf("%d", &cisloA);
+    scanf("%d", &cisloA); // add & operator to get the address of the variable
     printf("zadej druhe cislo:");
-    scanf("%d", &cisloB);
+    scanf("%d", &cisloB); // add & operator to get the address of the variable
 
-    switch (operace)
-    {
+    switch (operace) {
         case '+':
-            vysledek=cisloA+cisloB;
-            printf("vysledek je %f", vysledek);
-            break;
+            vysledek = cisloA + cisloB;
+            printf("vysledek je %f\n", vysledek); // add newline character
+            break; // add break statement to exit the switch block after each case
         case '-':
-            vysledek=cisloA-cisloB;
-            printf("vysledek je %f", vysledek);
-            break;
+            vysledek = cisloA - cisloB;
+            printf("vysledek je %f\n", vysledek); // add newline character
+            break; // add break statement to exit the switch block after each case
         case '*':
-            vysledek=cisloA*cisloB;
-            printf("vysledek je %f", vysledek);
-            break;
+            vysledek = cisloA * cisloB;
+            printf("vysledek je %f\n", vysledek); // add newline character
+            break; // add break statement to exit the switch block after each case
         case '/':
-            vysledek=(float)cisloA/(float)cisloB;
-            printf("vysledek je %f", vysledek);
-            break;
+            vysledek = (float) cisloA / (float) cisloB;
+            printf("vysledek je %f\n", vysledek); // add newline character
+            break; // add break statement to exit the switch block after each case
+        default:
+            printf("Chyba zadani.\n"); // add newline character
+    };
+
+    return 0;
+}
